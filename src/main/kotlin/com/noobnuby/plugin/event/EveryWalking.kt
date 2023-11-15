83dccf7a3bc7ab1d189dd7bedd3f0e1a9a6b0533
NOOBNUBY <noobnuby77@gmail.com>
2023-11-15 15:19:40 +0900

    fix ..

--- src/main/kotlin/com/noobnuby/plugin/event/EveryWalking.kt
+++ src/main/kotlin/com/noobnuby/plugin/event/EveryWalking.kt
@@ -11,32 +11,25 @@ import java.util.*
 class EveryWalking : Listener {
     val blocks: MutableList<Material> = ArrayList()
 
-    @EventHandler
-    fun onPlayerMove(e: PlayerMoveEvent) {
-        val p = e.player
-
+    init {
         for (block in Material.entries) {
             if (block.isBlock) {
                 blocks.add(block)
             }
         }
-
-        val x = p.location.x
-        val y = p.location.y
-        val z = p.location.z
-
-        if (isIntegerCoordinate(x) || isIntegerCoordinate(y) || isIntegerCoordinate(z)) {
-            if (e.getFrom().getX() != e.getTo().getX() || e.getFrom().getY() != e.getTo().getY() || e.getFrom().getZ() != e.getTo().getZ()) {
-                val pos = p.getLocation().subtract(0.0, 1.0, 0.0).block
-                if (!pos.isEmpty) {
-                    val randomBlock = blocks[Random().nextInt(blocks.size)]
-                    pos.setType(randomBlock)
-                }
-            }
-        }
     }
 
-    private fun isIntegerCoordinate(coordinate: Double): Boolean {
-        return coordinate == coordinate.toInt().toDouble()
+    @EventHandler
+    fun onPlayerMove(e: PlayerMoveEvent) {
+        val p = e.player
+
+        val pos = p.location.subtract(0.0, 1.0, 0.0)
+
+        if (pos.isBlock != e.hasChangedBlock()) {
+            if (!pos.block.isEmpty) {
+                val randomBlock = blocks[Random().nextInt(blocks.size)]
+                pos.block.type = randomBlock
+            }
+        }
     }
 }
